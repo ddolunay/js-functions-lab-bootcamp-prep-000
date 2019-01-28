e48a773bb202f48ffd6821f33655c69acf84d891
Ilkay Demir  Dolunay <demirdolunay@gmail.com>
2019-01-28 18:45:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function happyHolidays()
+function happyHolidays(happy,Holidays){
+  console.log ("happy", + "Holidays!")
+  
+}
 
 
 
 
+function okul (ad,sinif)
+ 
+{
+ 
+alert ( "Hoşgeldiniz " + ad + "," + sinif );
+ 
+}
